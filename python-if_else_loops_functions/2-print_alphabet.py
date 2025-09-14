e56b07f122de9c7bcb59f4944ed03ae1aaf5783a
lxcario <resquedzn05@gmail.com>
2025-09-14 16:57:12 +0000

    yes

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-<<<<<<< HEAD
     print("{}".format(chr(i)), end='')
-=======
-    print('{:c}'.format(i), end='')
->>>>>>> 8501556ee5f75d48afeacb11be8e6c7be0badf02
